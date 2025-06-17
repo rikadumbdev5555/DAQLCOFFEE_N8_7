598162bbf66ffdee3bd4212856a9d1fce523d438
rikadumbdev5555 <rikadumbdev@gmail.com>
2025-06-17 20:19:36 +0700

    Delete DAQLCOFFEEBKUP directory

--- DAQLCOFFEEBKUP/Java/thucdon.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll(".item");
-
-    items.forEach(item => {
-        item.addEventListener("click", () => {
-            alert(`Bạn đã chọn ${item.innerText.trim()}!`);
-        });
-    });
-});
